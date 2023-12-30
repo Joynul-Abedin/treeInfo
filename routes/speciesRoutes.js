7b77bf14a98640a10029ec156d3fabc5cf64aa18
Joynul Abedin <30983239+Joynul-Abedin@users.noreply.github.com>
2023-12-30 22:51:32 +0600

    Health Check Added

--- routes/speciesRoutes.js
+++ routes/speciesRoutes.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const { getSpecies, translateAndStore, fetchTreeWikiInfo, translate } = require('../controllers/speciesController');
 
+// Endpoint to test the API
+router.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
 router.get('/', getSpecies);
 router.get('/bangla', translateAndStore);
 router.get('/plantinfo', fetchTreeWikiInfo);
